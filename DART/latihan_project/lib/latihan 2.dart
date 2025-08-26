713f324c35f61e9e5e96307b68be76c056823607
Araninetenn <aryaandromeda3@gmail.com>
2025-08-26 12:54:11 +0900

    latihan 2.dart

---
+++ DART/latihan_project/lib/latihan 2.dart
@@ -0,0 +1,24 @@
+// void main(List<String> args) {
+//   double panjang = 10;
+//   double lebar = 5;
+
+//   double luas = panjang * lebar;
+//   double keliling = 2 * (panjang + lebar);
+
+//   print("panjang : $panjang");
+//   print("lebar : $lebar");
+//   print("luas : $luas");
+//   print("keliling : $keliling");
+// }
+
+void main(List<String> args) {
+  List<String> Buah = ["apel", "Pisang", "Pir", "jeruk", "mangga"];
+  print(Buah);
+  print(Buah[0]);
+  print(Buah[3]);
+  print(Buah[1]);
+
+  print(Buah.length);
+  Buah.addAll(["Semangga", "Apel"]);
+  print(Buah.indexOf("Pir"));
+}
--- DART/latihan_project/lib/latihan1.dart
+++ DART/latihan_project/lib/latihan1.dart
@@ -1,9 +1,13 @@
 void main() {
   String name = "Arya Andromeda";
-  // int usia = 20;
-  // double berat = 73;
-  // num tinggi_badan = 170;
-  // dynamic skill = "Makan"
+  int usia = 20;
+  double berat = 73;
+  num tinggi_badan = 170;
+  dynamic skill = "Makan";
 
   print(name);
+  print(usia);
+  print(berat);
+  print(tinggi_badan);
+  print(skill);
 }
